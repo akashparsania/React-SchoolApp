4f3ba70cb63af1cfd5c2748c7192a19fb1a6237c
AKASH PARSANIA <akashparsania@gmail.com>
2022-12-15 20:09:48 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import { Footer } from './common/Footer/Footer';
+import { Header } from './common/Header/Header'; 
+
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+      <Footer></Footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/reducer/appReducer.js
@@ -0,0 +1,8 @@
+import { init } from "../utilitis/init"
+export const appReducer=(state=init,action)=>{
+
+    switch(action.type){
+
+    }
+    return state;
+}
