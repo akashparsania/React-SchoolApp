e9c51113ffeab6997cfa0809153f17649cad98eb
AKASH PARSANIA <akashparsania@gmail.com>
2022-12-15 22:47:40 -0500

    added BeforeLogIn and AfterLogIn content

--- src/common/Footer/Footer.js
+++ src/common/Footer/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 import './Footer.css'
 export const Footer = () => {
   return (
-    <div className='Footer'> @copy Rights belongs to me</div>
+    <div className='Footer bg-primary text-center'> &copy; Rights belongs to me</div>
   )
 }
 
--- src/common/Header/Header.js
+++ src/common/Header/Header.js
@@ -2,7 +2,9 @@ import React from 'react'
 import './Header.css'
 export const Header = () => {
   return (
-    <div className='Header'>Header</div>
+    <div className='Header bg-primary text-center'>
+        (React+Redux) - Node.js -- Mongo
+        </div>
   )
 }
 
